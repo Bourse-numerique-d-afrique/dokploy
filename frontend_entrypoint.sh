5a592beb7774c0b9837e48b67453fc8fd3827ac0
princefr <pondonda@gmail.com>
2026-01-17 20:30:04 +0100

    change injection script

--- frontend_entrypoint.sh
+++ frontend_entrypoint.sh
@@ -1,38 +1,14 @@
 #!/bin/sh
+set -e
 
-# This script replaces environment variable placeholders in the built JS files at container startup.
-# This allows the same Docker image to be used across different environments (staging, production).
+: "${VITE_APP_SERVER_GRAPHQL_URL:?Missing VITE_APP_SERVER_GRAPHQL_URL}"
+: "${VITE_SERVER_GRAPHQL_WS_URL:?Missing VITE_SERVER_GRAPHQL_WS_URL}"
 
-ROOT_DIR=/usr/share/nginx/html
+cat <<EOF > /usr/share/nginx/html/env.js
+window.__ENV__ = {
+  GRAPHQL_HTTP: "${VITE_APP_SERVER_GRAPHQL_URL}",
+  GRAPHQL_WS: "${VITE_SERVER_GRAPHQL_WS_URL}"
+};
+EOF
 
-echo "Replacing environment variables in JS files..."
-
-# Find all JS files in the assets directory
-for file in $ROOT_DIR/assets/*.js;
-do
-  if [ -f "$file" ]; then
-    echo "Processing $file..."
-    
-    # Replace the GraphQL HTTP URL if the environment variable is set
-    # VITE_APP_SERVER_GRAPHQL_URL=https://api.boursenumeriquedafrique.com/graphql
-    if [ -n "$VITE_APP_SERVER_GRAPHQL_URL" ]; then
-      sed -i "s|http://localhost:5700/graphql|$VITE_APP_SERVER_GRAPHQL_URL|g" "$file"
-      sed -i "s|http://test-api.boursenumeriquedafrique.com/graphql|$VITE_APP_SERVER_GRAPHQL_URL|g" "$file"
-      # Specifically fix the 'gdh' string found in the image
-      sed -i "s|uri:\"gdh\"|uri:\"$VITE_APP_SERVER_GRAPHQL_URL\"|g" "$file"
-    fi
-    
-    # Replace the GraphQL WebSocket URL
-    # VITE_SERVER_GRAPHQL_WS_URL=wss://api.boursenumeriquedafrique.com/graphql/ws
-    if [ -n "$VITE_SERVER_GRAPHQL_WS_URL" ]; then
-      sed -i "s|ws://localhost:5700/graphql/ws|$VITE_SERVER_GRAPHQL_WS_URL|g" "$file"
-      sed -i "s|ws://test-api.boursenumeriquedafrique.com/graphql/ws|$VITE_SERVER_GRAPHQL_WS_URL|g" "$file"
-    fi
-
-     # Replace Payments URL
-     sed -i "s|test-payments.boursenumeriquedafrique.com|payments.boursenumeriquedafrique.com|g" "$file"
-  fi
-done
-
-echo "Environment variables replaced. Starting Nginx..."
-exec "$@"
+exec nginx -g "daemon off;"
